c5067a60ce092d5fd76c49d86b592add9a6eba26
GSiggins <gregory.siggins@gmail.com>
2022-07-14 10:43:27 -0700

    last minute changes

--- controllers/api/reviews.js
+++ controllers/api/reviews.js
@@ -7,44 +7,28 @@ const withAuth = require('../../utils/auth');
 
 // /theater/1
 
-router.get('/:id', async (req, res) => {
-    try {
-        // Get all projects and JOIN with user data
-        const reviewData = await Review.findAll({
-            where: {
-                theater_id: req.params.id
-            },
-        });
-
-        // Serialize data so the template can read it
-        const reviews = reviewData.map((Review) => Review.get({ plain: true }));
-
-        // Pass serialized data and session flag into template
-        res.render('reviews', {
-            reviews
-            // logged_in: req.session.logged_in 
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-// router.post('/', async (req, res) => {
+// router.get('/:id', async (req, res) => {
 //     try {
-//         console.log(req.body);
-//         const newReview = await Review.create({
-//             ...req.body,
-//             user_id: req.session.user_id,
+//         // Get all projects and JOIN with user data
+//         const reviewData = await Review.findAll({
+//             where: {
+//                 theater_id: req.params.id
+//             },
 //         });
 
-//         res.status(200).json(newReview);
+//         // Serialize data so the template can read it
+//         const reviews = reviewData.map((Review) => Review.get({ plain: true }));
+
+//         // Pass serialized data and session flag into template
+//         res.render('reviews', {
+//             reviews
+//             // logged_in: req.session.logged_in 
+//         });
 //     } catch (err) {
-//         console.log(err);
-//         res.status(400).json(err);
+//         res.status(500).json(err);
 //     }
 // });
 
-// route:   /api/reviews/:id
 
 router.delete('/:id', withAuth, async (req, res) => {
     try {
@@ -66,60 +50,5 @@ router.delete('/:id', withAuth, async (req, res) => {
     }
 });
 
-
-router.get('/:id', async (req, res) => {
-    try {
-        const seatingAvg = await Theater.findOne({
-            where: {
-                id: req.params.id
-            },
-            attributes: [
-                [sequelize.literal(
-                    '(SELECT AVG(seatingrating) FROM review)'
-                ),
-                    'avgSeatingRating'
-                ],
-                [sequelize.literal(
-                    '(SELECT AVG(concessionsrating) FROM review)'
-                ),
-                    'avgConcessionsRating'
-                ],
-                [sequelize.literal(
-                    '(SELECT AVG(audiorating) FROM review)'
-                ),
-                    'avgAudioRating'
-                ],
-                [sequelize.literal(
-                    '(SELECT AVG(videorating) FROM review)'
-                ),
-                    'avgVideoRating'
-                ],
-                [sequelize.literal(
-                    '(SELECT AVG(parkingrating) FROM review)'
-                ),
-                    'avgParkingRating'
-                ],
-                [sequelize.literal(
-                    '(SELECT AVG(servicerating) FROM review)'
-                ),
-                    'avgServiceRating'
-                ],
-                [sequelize.literal(
-                    '(SELECT AVG(crowdrating) FROM review)'
-                ),
-                    'avgCrowdRating'
-                ],
-                [sequelize.literal(
-                    '(SELECT AVG(crowdrating + servicerating + parkingrating + videorating + audiorating + concessionsrating + seatingrating)/7 FROM review)'
-                ),
-                    'masterRating'
-                ],
-            ]
-        })
-        res.json(seatingAvg);
-    } catch (err) {
-        res.status(400).json(err);
-    }
-})
 
 module.exports = router; 
